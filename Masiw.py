4883282d1c8729898e9e692592f1e66cc5978b41
SergeyGribkov <114168915+SergeyGribkov@users.noreply.github.com>
2022-11-25 12:05:52 +0300

    Add files via upload

---
+++ Masiw.py
@@ -0,0 +1,19 @@
+class Masiw:
+    a=None
+    def __init__ (self):
+        i=None
+    def __init__ (self, a='123456'):
+        self.a=a
+        def app(self):
+            c=int(input())
+            a.append(c)
+        def sr(self):
+            print(a[::1])
+        def ind(self):
+            d=3
+            print(a[2])
+a='123456'
+print(a)
+
+            
+                
